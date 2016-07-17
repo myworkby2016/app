ca77a06ad391210c071b80cc85a09f53df78ed85
zdwathena <zdwathena@gmail.com>
2016-07-17 16:09:58 +0900

    update

--- js/1-3-7.js
+++ js/1-3-7.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
 		window.location.href = './1-1.html';
 	});
 	$('#div_3').click(function() {
-		window.location.href = './1-11-1.html';
+		window.location.href = './1-13-1.html';
 	});
 	$('#div_4').click(function() {
-		window.location.href = './1-13-1.html';
+		window.location.href = './1-11-1.html';
 	});
 	$('#div_5').click(function() {
 

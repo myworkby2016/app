326d59fdfb3616220fc395270084c0d466508e0f
zdwathena <zdwathena@gmail.com>
2016-07-17 13:21:42 +0900

    add

---
+++ js/1-3-1.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$('#div_1').click(function() {
+		window.location.href = './1-3.html';
+	});
+	$('#div_2').click(function() {
+		window.location.href = './1-3-3.html';
+	});
+	$('#div_3').click(function() {
+		window.location.href = './1-3-5.html';
+	});
+	$('#div_4').click(function() {
+		window.location.href = './1-1.html';
+	});
+	$('#div_5').click(function() {
+		window.location.href = './1-11-1.html';
+	});
+	$('#div_6').click(function() {
+		window.location.href = './1-13-1.html';
+	});
+	$('#div_7').click(function() {
+		alert(7);
+	});
+	$('#div_8').click(function() {
+		alert(8);
+	});
+	$('#div_9').click(function() {
+		alert(9);
+	});
+	$('#div_10').click(function() {
+		alert(10);
+	});
+	$('#div_11').click(function() {
+		alert(11);
+	});
+	$('#div_12').click(function() {
+		alert(12);
+	});
+});
---
+++ js/1-3-3.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	$('#div_1').click(function() {
+		window.location.href = './1-3-1.html';
+	});
+	$('#div_2').click(function() {
+		window.location.href = './1-3-6.html';
+	});
+	$('#div_3').click(function() {
+		window.location.href = './1-1.html';
+	});
+	$('#div_4').click(function() {
+		window.location.href = './1-11-1.html';
+	});
+	$('#div_5').click(function() {
+		window.location.href = './1-13-1.html';
+	});
+	$('#div_6').click(function() {
+	});
+	$('#div_7').click(function() {
+	});
+	$('#div_8').click(function() {
+	});
+	$('#div_9').click(function() {
+	});
+	$('#div_10').click(function() {
+	});
+	$('#div_11').click(function() {
+	});
+	$('#div_12').click(function() {
+	});
+});

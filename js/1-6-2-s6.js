17fedd972ec5e4cc55b42c3f26fe7df7a610a412
quchengxin <quchengxin@outlook.com>
2016-07-20 22:40:29 +0800

    7/20 式样对应 qu

---
+++ js/1-6-2-s6.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $('#div_1').click(function() {
+		window.location.href = './1-6-2-s4.html';
+    });
+    $('#div_2').click(function() {
+		window.location.href = './1-6-2-s7.html';
+    });
+    $('#div_8').click(function() {
+ 		window.location.href = './1-1.html';
+    });
+    $('#div_9').click(function() {
+     	window.location.href = './1-13-1.html';
+    });
+    $('#div_10').click(function() {
+		window.location.href = './1-11-1.html';
+    });
+
+
+});

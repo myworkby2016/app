3d74de95256bebdc247006f59782c8ec89fd7b55
quchengxin <quchengxin@outlook.com>
2016-07-24 14:50:05 +0800

    7/24 照片追加  qu

---
+++ js/1-10-11.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#div_1').click(function() {
+		window.location.href = './1-10-1.html';
+    });
+
+    $('#div_3').click(function() {
+       window.location.href = './1-10-12.html';
+    });
+
+    $('#div_6').click(function() {
+		window.location.href = './1-1.html';
+    });
+    $('#div_7').click(function() {
+        window.location.href = './1-13-1.html';
+    });
+    $('#div_8').click(function() {
+        window.location.href = './1-11-1.html';
+    });
+});
